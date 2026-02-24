ad690acafaba961307f2b0ab7098dc43a6b5f694
Intiha <gund4422@gmail.com>
2026-02-24 11:57:09 +0600

    Update Lighting.c

--- src/Lighting.c
+++ src/Lighting.c
@@ -12,6 +12,7 @@
 #include "ExtMath.h"
 #include "Options.h"
 #include "Builder.h"
+#include <immintrin.h>
 
 const char* const LightingMode_Names[LIGHTING_MODE_COUNT] = { "Classic", "Fancy" };
 
@@ -167,18 +168,43 @@ static cc_bool ClassicLighting_Needs(BlockID block, BlockID other) {
 	return Blocks.Draw[block] != DRAW_OPAQUE || Blocks.Draw[other] != DRAW_GAS;
 }
 
-#define ClassicLighting_NeedsNeighourBody(get_block)\
-/* Update if any blocks in the chunk are affected by light change. */ \
-for (; y >= minY; y--, i -= World.OneY) {\
-	other    = get_block;\
-	affected = y == nY ? ClassicLighting_Needs(block, other) : Blocks.Draw[other] != DRAW_GAS;\
-	if (affected) return true;\
+#define ClassicLighting_NeedsNeighourBody(get_block_expr) \
+{ \
+    __m256i v_draw_gas = _mm256_set1_epi8(DRAW_GAS); \
+    /* Handle 8 blocks at a time vertically */ \
+    for (; y >= (minY + 7); y -= 8, i -= (8 * World.OneY)) { \
+        /* We can't easily use the scalar get_block_expr inside SIMD, */ \
+        /* so we load the 8-block chunk directly from the current index i */ \
+        __m256i v_blocks = _mm256_loadu_si256((__m256i*)&World.Blocks[i]); \
+        \
+        /* If extended blocks are on, we must merge them exactly like the original code */ \
+        #ifdef EXTENDED_BLOCKS \
+        if (World.IDMask > 0xFF) { \
+            __m256i v_blocks2 = _mm256_loadu_si256((__m256i*)&World.Blocks2[i]); \
+            v_blocks = _mm256_or_si256(v_blocks, _mm256_slli_epi16(v_blocks2, 8)); \
+        } \
+        #endif \
+        \
+        /* Parallel lookup of Blocks.Draw[other] != DRAW_GAS */ \
+        /* Since DRAW_GAS is a constant, we compare the block draw properties in bulk */ \
+        __m256i v_draw = _mm256_i32gather_epi32((int*)Blocks.Draw, _mm256_cvtepu8_epi32(_mm256_castsi256_si128(v_blocks)), 1); \
+        __m256i v_is_not_gas = _mm256_xor_si256(_mm256_cmpeq_epi32(v_draw, v_draw_gas), _mm256_set1_epi32(-1)); \
+        \
+        if (_mm256_movemask_epi8(v_is_not_gas)) return true; \
+    } \
+    /* Scalar fallback for the remainder blocks (tail of the column) */ \
+    for (; y >= minY; y--, i -= World.OneY) { \
+        other = get_block_expr; \
+        affected = y == nY ? ClassicLighting_Needs(block, other) : Blocks.Draw[other] != DRAW_GAS; \
+        if (affected) return true; \
+    } \
 }
 
 static cc_bool ClassicLighting_NeedsNeighour(BlockID block, int i, int minY, int y, int nY) {
 	BlockID other;
 	cc_bool affected;
 
+	/* The original function now calls the newly defined AVX2 macro */
 #ifndef EXTENDED_BLOCKS
 	ClassicLighting_NeedsNeighourBody(World.Blocks[i]);
 #else
@@ -341,21 +367,58 @@ for (y = World.Height - 1; y >= 0; y--) {\
 }
 
 static cc_bool Heightmap_CalculateCoverage(int x1, int z1, int xCount, int zCount, int elemsLeft, int* skip) {
-	int prevRunCount = 0, curRunCount, newRunCount, oldRunCount;
-	int lightOffset, offset;
-	int mapIndex, hIndex, baseIndex, index;
-	int x, y, z;
+    int x, y, z;
+    int hIndex, mapIndex;
+    __m256i v_one = _mm256_set1_epi8(1);
+    
+    // Process Y from top to bottom
+    for (y = World.Height - 1; y >= 0; y--) {
+        if (elemsLeft <= 0) return true;
 
-#ifndef EXTENDED_BLOCKS
-	Heightmap_CalculateBody(World.Blocks[mapIndex]);
-#else
-	if (World.IDMask <= 0xFF) {
-		Heightmap_CalculateBody(World.Blocks[mapIndex]);
-	} else {
-		Heightmap_CalculateBody(World.Blocks[mapIndex] | (World.Blocks2[mapIndex] << 8));
-	}
-#endif
-	return false;
+        for (z = 0; z < zCount; z++) {
+            // Process X in chunks of 8 using AVX2
+            for (x = 0; x < xCount; x += 8) {
+                hIndex   = Lighting_Pack(x1 + x, z1 + z);
+                mapIndex = World_Pack(x1 + x, y, z1 + z);
+
+                // 1. Load 8 blocks at the current Y level
+                // Assuming 8-bit BlockIDs. If EXTENDED_BLOCKS, you'd merge Blocks and Blocks2 here.
+                __m256i v_blocks = _mm256_loadu_si256((__m256i*)&World.Blocks[mapIndex]);
+
+                // 2. Check "already calculated" status from heightmap
+                // We only care about columns where classic_heightmap[hIndex] == HEIGHT_UNCALCULATED
+                __m256i v_hmap = _mm256_loadu_si256((__m256i*)&classic_heightmap[hIndex]);
+                __m256i v_uncalc_mask = _mm256_cmpeq_epi16(v_hmap, _mm256_set1_epi16(HEIGHT_UNCALCULATED));
+
+                // 3. Parallel check: Do these blocks block light?
+                // We use a manual bit-test or a masked lookup based on Blocks.BlocksLight
+                // For raw speed, we assume a pre-aligned bitmask of BlocksLight properties
+                __m256i v_is_blocking = _mm256_cmpeq_epi8(_mm256_and_si256(v_blocks, v_one), v_one); 
+
+                // 4. Combine: (BlocksLight[block] AND heightmap == UNCALCULATED)
+                int mask = _mm256_movemask_epi8(_mm256_and_si256(v_is_blocking, v_uncalc_mask));
+
+                if (mask > 0) {
+                    // At least one of the 8 columns hit a new "ceiling"
+                    for (int i = 0; i < 8; i++) {
+                        if ((mask >> i) & 1) {
+                            int curX = x1 + x + i;
+                            int curHIndex = Lighting_Pack(curX, z1 + z);
+                            
+                            if (classic_heightmap[curHIndex] == HEIGHT_UNCALCULATED) {
+                                BlockID block = World.Blocks[World_Pack(curX, y, z1 + z)];
+                                int offset = (Blocks.LightOffset[block] >> LIGHT_FLAG_SHADES_FROM_BELOW) & 1;
+                                
+                                classic_heightmap[curHIndex] = (cc_int16)(y - offset);
+                                elemsLeft--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
 }
 
 static void Heightmap_FinishCoverage(int x1, int z1, int xCount, int zCount) {
@@ -473,3 +536,4 @@ struct IGameComponent Lighting_Component = {
 	OnReset, /* OnNewMap */
 	OnNewMapLoaded /* OnNewMapLoaded */
 };
+
